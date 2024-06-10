d5f556701ab8d31863c0af6eac5fb67502c6cf69
Igor Gilmutdinov <bladoff@gmail.com>
2024-06-10 14:39:07 +0300

    fix: upload form import

--- hexatube/src/components/Dashboard/Dashboard.jsx
+++ hexatube/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import UploadForm from '../UploadForm/UploadForm';
+import UploadForm from '../Upload/Upload';
 
 const Dashboard = () => {
     const [videos, setVideos] = useState([]);
